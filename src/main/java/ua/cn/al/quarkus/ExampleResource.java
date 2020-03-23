20317297c2175f60ba6281aacdc26be383ccfa4b
Oleksiy Lukin <al@alhp.alhome>
2020-03-23 08:53:46 +0200

    first commit

---
+++ src/main/java/ua/cn/al/quarkus/ExampleResource.java
@@ -0,0 +1,25 @@
+package ua.cn.al.quarkus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.proxy.*;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        String out = "From JS:";
+        try (Context context = Context.create()) {
+            Value function = context.eval("js", "x => x+1");
+            assert function.canExecute();
+            int x = function.execute(41).asInt();
+            out=out+x;
+        }
+        return out;
+    }
+}
